890cc36563045c23d7bf69419ddf298d0d3a605b
noamsabb <sabban.noam@gmail.com>
2026-02-15 23:29:15 +0200

    deployment frontend fixs

---
+++ Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  employeesUrl: "https://geostaff-backend.onrender.com/api/employees/",
+  randomLocUrl: "https://geostaff-backend.onrender.com/api/employees/random-loc",
+};
