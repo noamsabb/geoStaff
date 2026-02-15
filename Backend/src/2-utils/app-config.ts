7a059642965fd3f47c41a89faa2be46e33d35ab5
noamsabb <sabban.noam@gmail.com>
2026-02-15 23:16:28 +0200

    deployment fixs

--- Backend/src/2-utils/app-config.ts
+++ Backend/src/2-utils/app-config.ts
@@ -1,14 +1,12 @@
-import dotenv from "dotenv"; // npm i dotenv
+import dotenv from "dotenv";
 
-// Load .env file into process.env object.
 dotenv.config({ quiet: true });
 
 class AppConfig {
   public readonly isDevelopment = process.env.ENVIRONMENT === "development";
   public readonly isProduction = process.env.ENVIRONMENT === "production";
-  public readonly port = 4000;
-  public readonly mongodbConnectionString =
-    process.env.MONGODB_CONNECTION_STRING!;
+  public readonly port = Number(process.env.PORT) || 4000; 
+  public readonly mongodbConnectionString = process.env.MONGODB_CONNECTION_STRING!;
 }
 
 export const appConfig = new AppConfig();
